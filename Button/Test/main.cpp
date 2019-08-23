7087b2d0e30600655f49b2c5b6647d6eec19d28a
Qt君 <2088201923@qq.com>
2019-08-23 22:59:21 +0800

    添加测试程序图标;完善button.qss

--- Button/Test/main.cpp
+++ Button/Test/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     MainWindow w;
+    w.setWindowTitle(QString::fromLocal8Bit("ButtonQSS by Qt君"));
     w.show();
 
     QFile file("../Button.qss");
--- Button/Test/mainwindow.cpp
+++ Button/Test/mainwindow.cpp
@@ -11,6 +11,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->pushButton_3->setObjectName("RedButton");
     ui->pushButton_4->setObjectName("GrayButton");
     ui->pushButton_5->setObjectName("BlackButton");
+
+    ui->pushButton_6->setObjectName("GreenButton2");
+    ui->pushButton_7->setObjectName("BlueButton2");
+    ui->pushButton_8->setObjectName("RedButton2");
+    ui->pushButton_9->setObjectName("GrayButton2");
+    ui->pushButton_10->setObjectName("BlackButton2");
 }
 
 MainWindow::~MainWindow()

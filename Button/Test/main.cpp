377cb2a9e5bdb6967b32db7496a1a01eab13bffd
Qt君 <2088201923@qq.com>
2019-08-23 22:13:30 +0800

    创建基础代码

---
+++ Button/Test/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.show();
+
+    QFile file("../Button.qss");
+    file.open(QIODevice::ReadOnly);
+    a.setStyleSheet(file.readAll());
+
+    return a.exec();
+}

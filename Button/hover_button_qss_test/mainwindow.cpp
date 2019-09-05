94a34af38955f442442dd3d2f825b2171e7ad42d
Qt君 <2088201923@qq.com>
2019-09-06 00:44:03 +0800

    调整目录

---
+++ Button/hover_button_qss_test/mainwindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->pushButton->setObjectName("GreenButton");
+    ui->pushButton_2->setObjectName("BlueButton");
+    ui->pushButton_3->setObjectName("RedButton");
+    ui->pushButton_4->setObjectName("GrayButton");
+    ui->pushButton_5->setObjectName("BlackButton");
+
+    ui->pushButton_6->setObjectName("GreenButton2");
+    ui->pushButton_7->setObjectName("BlueButton2");
+    ui->pushButton_8->setObjectName("RedButton2");
+    ui->pushButton_9->setObjectName("GrayButton2");
+    ui->pushButton_10->setObjectName("BlackButton2");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ Button/hover_button_qss_test/test.pro
@@ -0,0 +1,27 @@
+#**********************************************************
+# Author: Qt君
+# 微信公众号: Qt君
+# QQ群: 732271126
+# Email: 2088201923@qq.com
+# LICENSE: MIT
+#**********************************************************
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ButtonQssTest
+TEMPLATE = app
+
+win32: RC_ICONS = app.ico
+
+CONFIG += c++11
+
+SOURCES += \
+        main.cpp \
+        mainwindow.cpp
+
+HEADERS += \
+        mainwindow.h
+
+FORMS += \
+        mainwindow.ui
